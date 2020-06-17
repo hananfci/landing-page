ff7fe9a370e85b7700edc5e855d5a7a1c8c09472
Hanan Mostafa <36992480+hananfci@users.noreply.github.com>
2020-06-17 21:12:24 +0200

    updates comments

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
 # Landing Page Project
 
+
 ## Table of Contents
 
+* [Description](#description)
 * [Instructions](#instructions)
+* [Styles](#styles)
+
+## Description
+
+This project is build on using HTML, CSS and Vanilla JavaScript to build dymanic  menu items and to toggle active class item menu by 
+ modifying the HTML and CSS files and app.js file
+
+ get started from  `js/app.js`
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+To start the application, click on index.html page to run page on browser
 
-To get started, open `js/app.js` and start building out the app's functionality
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+## Styles
+
+This project follows  WebStorm coding styles.
+
+
--- js/app.js
+++ js/app.js
@@ -1,48 +1,10 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
 
 $(function(){
 
-    /**
- * Define Global Variables
- * 
+  /**
+* Global Variables
+* 
 */
 const newli = document.createElement('li');
 const navbarlist = document.getElementById("navbar__list");
@@ -50,55 +12,56 @@ const sections = Array.prototype.slice.call(document.getElementsByTagName('secti
 const numsection= sections.length;
 console.log(sections)
 /**
- * End Global Variables
- * Start Helper Functions
- * 
+* End Global Variables
+* Start scrollto Functions
+* 
 */
 
 function scrollto(position){
-  debugger;
+debugger;
 
-  var elr = document.getElementById(position);
-  $("html, body").animate({ scrollTop: $(elr ).offset().top }, 'slow');
-   //elmnt.scrollIntoView();
+var elr = document.getElementById(position);
+
+// Scroll to section on link click
+elr.scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
-  function rendermenu () {
-    const homeli = document.createElement('li');
-    homeli.innerText = 'Home';
-    homeli.classList.add("menu__link", "navbar-active");    
-    homeli.addEventListener('click', function (e) {
-      document.querySelector('.navbar-active').classList.remove('navbar-active')
-      e.target.classList.add('navbar-active')
-      scrollto("home")
-     });
-   
-    navbarlist.appendChild(homeli);
-    sections.forEach(section => {
-        
-       const elmid = section.id;
-       const value = $('#'+elmid).attr('data-nav')
-       console.log(value);
-       const newli = document.createElement('li');
-        newli.innerText = value.toUpperCase();        
-        newli.className="menu__link";
+/* build dynamic menu items */
+function rendermenu () {
+  const homeli = document.createElement('li');
+  homeli.innerText = 'Home';
+// Set Home sections as active
+  homeli.classList.add("menu__link", "navbar-active");    
+  homeli.addEventListener('click', function (e) {
+    document.querySelector('.navbar-active').classList.remove('navbar-active')
+    e.target.classList.add('navbar-active')
+    scrollto("home")
+   });
+ 
+  navbarlist.appendChild(homeli);
+  sections.forEach(section => {
+      
+     const elmid = section.id;
+     const value = $('#'+elmid).attr('data-nav')
+     console.log(value);
+     const newli = document.createElement('li');
+      newli.innerText = value.toUpperCase();        
+      newli.className="menu__link";
+// Set sections as active
+      newli.addEventListener('click', function (e) {
+        document.querySelector('.navbar-active').classList.remove('navbar-active')
+        e.target.classList.add('navbar-active')
+       scrollto(elmid)
        
-        newli.addEventListener('click', function (e) {
-          document.querySelector('.navbar-active').classList.remove('navbar-active')
-          e.target.classList.add('navbar-active')
-         scrollto(elmid)
-         
-    });
+  });
 
-        navbarlist.appendChild(newli);
+      navbarlist.appendChild(newli);
 });
-    
-   }
-   rendermenu()
+  
+ }
+ rendermenu()
 
 })
 
-// Scroll to section on link click
 
-// Set sections as active
 
 
